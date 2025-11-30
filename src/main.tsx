cd2530d53e5b729ba7e12593da254d7f496c55bd
biki-chg <v_bmain@mychg.com>
2025-11-29 17:04:26 -0800

    refactor build config

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import './index1.css'
 import { RouterProvider } from 'react-router'
 import { index } from './routes'
 
