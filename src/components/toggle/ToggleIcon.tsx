e81690b35d6a1c0841a5e6419e1e34ddae942cca
biki-chg <v_bmain@mychg.com>
2025-12-24 21:35:47 -0800

    css:fix

--- src/components/toggle/ToggleIcon.tsx
+++ src/components/toggle/ToggleIcon.tsx
@@ -1,14 +1,10 @@
 import './ToggleIcon.css'
 
-export default function ToggleIcon({isLight}: {isLight: boolean }) {
+export default function ToggleIcon({isLight}: { isLight: boolean }) {
 
     return (
-        <div>
-            <button className="App">
-                <button className={`toggle-button ${!isLight ? 'dark' : ''}`}>
-                    <div className="thumb"></div>
-                </button>
-            </button>
+        <div className={`toggle-button ${!isLight ? 'dark' : ''}`}>
+            <div className="thumb"></div>
         </div>
     )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,28 @@
-// src/routes/index.tsx
-import { createHashRouter } from 'react-router-dom'
+import {createHashRouter} from 'react-router-dom'
 import RootLayout from './RootLayout'
 import ErrorPage from './ErrorPage'
-import { Home } from '../pages/Home'
-import { Contact } from '../pages/Contact'
-import { Services } from '../pages/Services'
-import { Blogs } from '../pages/Blogs'
-import { Gallery } from '../pages/Gallery'
-import { Janakari } from '../pages/Janakari'
-import { About } from '../pages/About.tsx'
+import {Home} from '../pages/Home'
+import {Contact} from '../pages/Contact'
+import {Services} from '../pages/Services'
+import {Blogs} from '../pages/Blogs'
+import {Gallery} from '../pages/Gallery'
+import {Janakari} from '../pages/Janakari'
+import {About} from '../pages/About.tsx'
 
 export const index = createHashRouter([
     {
         path: '/',
-        element: <RootLayout />,
-        errorElement: <ErrorPage />,
+        element: <RootLayout/>,
+        errorElement: <ErrorPage/>,
         children: [
-            { index: true, element: <Home /> },
-            { path: 'about', element: <About /> },
-            { path: 'contact', element: <Contact /> },
-            { path: 'services', element: <Services /> },
-            { path: 'blogs', element: <Blogs /> },
-            { path: 'gallery', element: <Gallery /> },
-            { path: 'janakari', element: <Janakari /> },
+            {index: true, element: <Home/>},
+            {path: 'about', element: <About/>},
+            {path: 'contact', element: <Contact/>},
+            {path: 'services', element: <Services/>},
+            {path: 'blogs', element: <Blogs/>},
+            {path: 'gallery', element: <Gallery/>},
+            {path: 'janakari', element: <Janakari/>}
         ],
     },
 ])
+
